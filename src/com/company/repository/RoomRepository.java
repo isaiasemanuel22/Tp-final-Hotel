8f1b573a6239895ac6d4a26d151c4f34c1919d67
Isaias <isaiasemanuelcalfin@hotmail.com>
2021-05-31 21:31:43 -0300

    creacion de roomRepository

---
+++ src/com/company/repository/RoomRepository.java
@@ -0,0 +1,35 @@
+package com.company.repository;
+
+import com.company.models.Room;
+import com.company.utils.Archivos;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RoomRepository {
+
+    Archivos<Room> archivos = new Archivos<>("Rooms");
+
+    public void RoomsGenerate() throws IOException {
+        ArrayList<Room> rooms = new ArrayList<>();
+        if(archivos.existFile()){
+            rooms = archivos.readMany(Room.class);
+        }else{
+            for(int i = 0 ; i < 40 ; i++){
+
+            }
+        }
+    }
+
+
+    public ArrayList<Room> rooms(int option , int cant){
+        ArrayList<Room> roomsNueva = new ArrayList<>();
+
+
+
+        return roomsNueva;
+    }
+
+
+
+}
