b001f47c25fce7646905c6b05dc7f5ef44ac2360
ivan <ivanahuel.ing@gmail.com>
2021-06-03 14:29:12 -0300

    Repository y Service de 'Reservation'

---
+++ src/com/company/services/ReservationService.java
@@ -0,0 +1,20 @@
+package com.company.services;
+
+import com.company.controller.Menu;
+import com.company.models.Reservation;
+
+import java.util.ArrayList;
+
+public class ReservationService {
+
+    Menu requestsToUser = new Menu();
+
+    public void createReservation() {
+
+        // pedir tipo de habitacion y fechas
+
+        // verifico que haya disponibles
+
+
+    }
+}
